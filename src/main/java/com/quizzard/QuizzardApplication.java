8cccac4d9af69425e438ce5f1c1dc5d5d79758be
FonieFones <christophermauricci@gmail.com>
2018-08-21 11:07:11 +0200

    Add .gitignore

---
+++ src/main/java/com/quizzard/QuizzardApplication.java
@@ -0,0 +1,12 @@
+package com.quizzard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzardApplication.class, args);
+    }
+}
