a5183fa92b7ef5cc79d36deffb39eb3dda19e79d
victor m <victor.moback@gmail.com>
2018-08-21 15:30:38 +0200

    added datasource autowired

--- src/main/java/com/quizzard/Repository/JDBCQuizRepository.java
+++ src/main/java/com/quizzard/Repository/JDBCQuizRepository.java
@@ -1,7 +1,19 @@
 package com.quizzard.Repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 @Component
 public class JDBCQuizRepository implements QuizRepository {
+
+    @Qualifier("dataSource")
+    @Autowired
+    private DataSource dataSource;
+
+
+
+
 }
